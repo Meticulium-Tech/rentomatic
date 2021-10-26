52483599e1f300f11b8dec3350856dfc12acb16e
chen237 <chenxhenor@gmail.com>
2021-10-26 17:13:05 +0100

    starter

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:rentomatic/controllers/user_controller.dart';
 import 'package:rentomatic/models/models.dart';
-import 'package:rentomatic/providers/custom_quiz_question_provider.dart';
 import 'package:rentomatic/providers/providers.dart';
 import 'package:rentomatic/utils/utils.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,62 +10,62 @@ class Database {
   final Reader _read;
   Database(this._read);
 
-  Future<List<Question>> getQuestions(
-      // {
-      // int? numQuestions,
-      // String? categoryId,
-      // String? level,
-      // }
-      ) async {
-    print("this is the subject category ${_read(quizSubjectProvider).state}");
-    print("this the level of student ${_read(studentControllerProvider.notifier).state.level}");
-    print("finally the number question ${_read(quizQuestionNumberProvider)}");
-    await _firestore
-        .collection('questions')
-        .where('category', isEqualTo: _read(quizSubjectProvider).state)
-        .where('level',
-            isEqualTo: _read(studentControllerProvider.notifier).state.level)
-        .limit(_read(quizQuestionNumberProvider).state)
-        .get()
-        .then((value) async {
-      if (value.docs.isNotEmpty) {
-        print("successfully entered the database ");
+  // Future<List<Question>> getQuestions(
+  //     // {
+  //     // int? numQuestions,
+  //     // String? categoryId,
+  //     // String? level,
+  //     // }
+  //     ) async {
+  //   print("this is the subject category ${_read(quizSubjectProvider).state}");
+  //   print("this the level of student ${_read(studentControllerProvider.notifier).state.level}");
+  //   print("finally the number question ${_read(quizQuestionNumberProvider)}");
+  //   await _firestore
+  //       .collection('questions')
+  //       .where('category', isEqualTo: _read(quizSubjectProvider).state)
+  //       .where('level',
+  //           isEqualTo: _read(studentControllerProvider.notifier).state.level)
+  //       .limit(_read(quizQuestionNumberProvider).state)
+  //       .get()
+  //       .then((value) async {
+  //     if (value.docs.isNotEmpty) {
+  //       print("successfully entered the database ");
 
-        value.docs.forEach((e) {
-          final myQuestion = Question(
-            correctAnswer: e['correct_answer'] ?? '',
-            question: e['question'] ?? '',
-            category: e['category'] ?? '',
-            answers: List<String>.from(e['incorrect_answers'])
-              ..add(e['correct_answer'] ?? '')
-              ..shuffle(),
-            level: e['level'] ?? '',
-          );
-          print(myQuestion.toString());
-          _read(customQuizQuestionProvider.notifier).state.add(myQuestion);
-        });
-        print(
-            " the state after database insertion ${_read(customQuizQuestionProvider.notifier).state.toString()}");
-        return value.docs.map((e) {
-          final myQuestion = new Question(
-            correctAnswer: e['correct_answer'] ?? '',
-            question: e['question'] ?? '',
-            category: e['category'] ?? '',
-            answers: List<String>.from(e['incorrect_answers'])
-              ..add(e['correct_answer'] ?? '')
-              ..shuffle(),
-            level: e['level'] ?? '',
-          );
-          print(myQuestion.toString());
-          return myQuestion;
-        }).toList();
-      } else {
-        print("The document was not found");
-      }
-    });
+  //       value.docs.forEach((e) {
+  //         final myQuestion = Question(
+  //           correctAnswer: e['correct_answer'] ?? '',
+  //           question: e['question'] ?? '',
+  //           category: e['category'] ?? '',
+  //           answers: List<String>.from(e['incorrect_answers'])
+  //             ..add(e['correct_answer'] ?? '')
+  //             ..shuffle(),
+  //           level: e['level'] ?? '',
+  //         );
+  //         print(myQuestion.toString());
+  //         _read(customQuizQuestionProvider.notifier).state.add(myQuestion);
+  //       });
+  //       print(
+  //           " the state after database insertion ${_read(customQuizQuestionProvider.notifier).state.toString()}");
+  //       return value.docs.map((e) {
+  //         final myQuestion = new Question(
+  //           correctAnswer: e['correct_answer'] ?? '',
+  //           question: e['question'] ?? '',
+  //           category: e['category'] ?? '',
+  //           answers: List<String>.from(e['incorrect_answers'])
+  //             ..add(e['correct_answer'] ?? '')
+  //             ..shuffle(),
+  //           level: e['level'] ?? '',
+  //         );
+  //         print(myQuestion.toString());
+  //         return myQuestion;
+  //       }).toList();
+  //     } else {
+  //       print("The document was not found");
+  //     }
+  //   });
 
-    return [];
-  }
+  //   return [];
+  // }
 
   Future<bool> createUser(
       {required String id, required Student student}) async {
